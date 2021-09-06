78115be83d676a4acf7619afcb22a087c932dded
Dipali Gunjal <60472255+dipali57@users.noreply.github.com>
2021-09-06 11:56:07 +0530

    user defined header file

---
+++ Header.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int BinaryToDecimal(int);
+int BinaryToOctal(int);
+void BinaryToHexadecimal(int);
+void DecimalToBinary(int);
+void DecimalToOctal(int);
+void DecimalToHexadecimal(int);
+void OctalToBinary(int);
+void OctalToDecimal(int);
+
